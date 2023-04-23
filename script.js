01f45e43265380a836968ebb966d7ad186321f69
Leouonline <leoucu@gmail.com>
2023-04-23 10:12:58 +0100

    first

---
+++ script.js
@@ -0,0 +1,26 @@
+function love() {
+  var name1 = document.getElementById('NameOne').value;
+  var name2 = document.getElementById('NameTwo').value;
+
+  var random = Math.floor(Math.random() * 100);
+  window.localStorage.setItem('items', JSON.stringify({ score: random }));
+
+  if (name1.length <= 2) {
+    alert('Enter at least 3 character');
+  } else if (name2.length <= 2) {
+    alert('Enter at least 3 character');
+  } else if (random <= 40) {
+    window.location.assign('low.html');
+    // document.getElementById('statement').innerHTML = 'Bad Relationship';
+  } else if (random <= 41 || random <= 80) {
+    window.location.assign('mid.html');
+    // document.getElementById('statement').innerHTML = 'Good Relationship';
+  } else {
+    window.location.assign('high.html');
+    // document.getElementById('statement').innerHTML = 'Perfect Relationship';
+  }
+}
+
+function love2() {
+  window.location.assign('index.html');
+}
